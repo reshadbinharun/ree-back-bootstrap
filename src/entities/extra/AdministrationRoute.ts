5ae43bdbd734c8174adacfbd20ea6abcc50f7792
reshadbinharun <md.bin_harun@tufts.edu>
2019-09-08 11:45:33 -0400

    moved entities to extra

---
+++ src/entities/extra/AdministrationRoute.ts
@@ -0,0 +1,21 @@
+import {
+    Entity,
+    BaseEntity,
+    Column,
+    PrimaryGeneratedColumn,
+    ManyToMany,
+} from "typeorm";
+
+import DevelopmentStatusSummary from "../DevelopmentStatusSummary"
+
+@Entity("administrationRoute")
+export default class AdministrationRoute extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    administrationRouteName: string;
+
+    @ManyToMany(type => DevelopmentStatusSummary, developmentStatusSummary => developmentStatusSummary.administrationRoutes, { nullable: true, cascade: true})
+    developmentStatusSummaries: DevelopmentStatusSummary[];
+}
